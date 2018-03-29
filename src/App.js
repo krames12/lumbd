0629cf42bd310ed443a0f7942003118888f4818d
Nick Kramer <nkramer@cwims.com>
2018-03-29 10:40:53 -0400

    now fetches from moviedb.org. key will be cancelled, dont try it

--- src/App.js
+++ src/App.js
@@ -4,30 +4,31 @@ import './App.css';
 
 import Movie from './movie';
 
-const movies = [
-  {
-    id: 1,
-    title: "Deadpool",
-    desc: "Death and not giving a damn"
-  },
-  {
-    id: 2,
-    title: "Avengers"
-  },
-  {
-    id: 3,
-    title: "Shawshank Redemption"
-  },
-];
-
 class App extends Component {
+
+  state = {
+    movies: []
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch("https://api.themoviedb.org/3/discover/movie?api_key=acc0e39aa4ce6d9d7324cf2db6807084&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1");
+      const movies = await res.json();
+      this.setState({
+        movies: movies.results
+      })
+    } catch(err) {
+      console.warn(err);
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        {movies.map(movie => <Movie key={movie.id} movie={movie} desc={movie.desc} />)}
+        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
       </div>
     );
   }
