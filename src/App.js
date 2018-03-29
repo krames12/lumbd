61f901b81b1d5447b777d44bd49f97cbb69290ec
Nick Kramer <nkramer@cwims.com>
2018-03-29 09:40:34 -0400

    did work prior, forgot to commit.
    Currently looping over movies array and just created first sub component

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Movie from './movie';
+
+const movies = [
+  {
+    id: 1,
+    title: "Deadpool"
+  },
+  {
+    id: 2,
+    title: "Avengers"
+  },
+  {
+    id: 3,
+    title: "Shawshank Redemption"
+  },
+];
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        {movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+      </div>
+    );
+  }
+}
+
+export default App;
